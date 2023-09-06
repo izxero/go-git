e265872e7d5a59cccaab681da0e542b451486368
izxero <34979074+izxero@users.noreply.github.com>
2023-09-06 21:03:27 +0700

    -refactor :package: refactor message in main.go

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Git")
+	fmt.Println("Hello Github")
 }
+
+//1 add . = add files
+//2 git commit -m "message"  = commit files
+//3 git push origin master = push files to github
