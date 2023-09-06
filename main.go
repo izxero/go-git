cf5a6cff0f8cad10b96b8a21b6b0a20a4a117610
izxero <34979074+izxero@users.noreply.github.com>
2023-09-06 21:22:38 +0700

    -feature :sparkles: add mul

--- main.go
+++ main.go
@@ -9,7 +9,13 @@ import "fmt"
 func sum (a,b int) int{
 	return a+b
 }
+
+func mul(a,b int)int{
+	return a*b
+}
+
 func main() {
 	fmt.Println("Hello Github")
 	fmt.Println(sum(1,2))
+	fmt.Println(mul(1,2))
 }
