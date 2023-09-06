8a2bbcb49223606dd96c3bce317a9534e176efa2
izxero <34979074+izxero@users.noreply.github.com>
2023-09-06 21:28:50 +0700

    -feature :sparkles: add display

--- main.go
+++ main.go
@@ -21,10 +21,15 @@ func div(a,b float64)float64{
 	return a/b
 }
 
+func display(msg string){
+	fmt.Println(msg)
+}
+
 func main() {
 	fmt.Println("Hello Github")
 	fmt.Println(sum(1,2))
 	fmt.Println(sub(1,2))
 	fmt.Println(mul(1,2))
 	fmt.Println(div(1,2))
+	display("Hello Gitflow")
 }
