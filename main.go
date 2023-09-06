5aa6ec66b856fc61e4a9cad42bcfc45141081807
izxero <34979074+izxero@users.noreply.github.com>
2023-09-06 21:17:24 +0700

    -feature :sparkles: sum completed

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello Github")
-}
-
 //1 add . = add files
 //2 git commit -m "message"  = commit files
 //3 git push origin master = push files to github
+
+func sum (a,b int) int{
+	return a+b
+}
+func main() {
+	fmt.Println("Hello Github")
+	fmt.Println(sum(1,2))
+}
