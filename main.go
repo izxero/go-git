c054644445684924eb4f987bf3cc75c0f4a1dd2b
izxero <34979074+izxero@users.noreply.github.com>
2023-09-06 21:21:15 +0700

    -feature :sparkles: add sub func

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import "fmt"
 //1 add . = add files
 //2 git commit -m "message"  = commit files
 //3 git push origin master = push files to github
-
+func sub(a,b int) int{
+	return a-b
+}
 func sum (a,b int) int{
 	return a+b
 }
 func main() {
 	fmt.Println("Hello Github")
 	fmt.Println(sum(1,2))
+	fmt.Println(sub(1,2))
 }
